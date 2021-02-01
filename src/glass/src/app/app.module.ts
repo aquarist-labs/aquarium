2cee8f36f6dc07378c0d935e387ac70e79e66480
Volker Theile <vtheile@suse.com>
2021-02-01 17:48:50 +0100

    glass: Implement basic framework
    Fixes: https://github.com/aquarist-labs/planning-and-discussion/issues/15
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/glass/src/app/app.module.ts
+++ src/glass/src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { MaterialModule } from './material.modules';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, MaterialModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
