8dc4acb2ef58b61aa3220008de69c0987bcad66c
Joao Eduardo Luis <joao@suse.com>
2021-02-04 17:38:08 -0100

    gravel: add config class
    Will keep an up-to-date file in /etc/aquarium/
    
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- src/gravel/controllers/gstate.py
+++ src/gravel/controllers/gstate.py
@@ -3,15 +3,19 @@ import asyncio
 from concurrent.futures.thread import ThreadPoolExecutor
 from typing import Callable, Any
 
+from gravel.controllers.config import Config
+
 
 class GlobalState:
 
     executor: ThreadPoolExecutor
     counter: int
+    config: Config
 
     def __init__(self):
         self.executor = ThreadPoolExecutor()
         self.counter = 0
+        self.config = Config()
         pass
 
     async def run_in_background(self, func: Callable, *args: Any):
