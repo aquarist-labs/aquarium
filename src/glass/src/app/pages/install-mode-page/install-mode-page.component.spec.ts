59b8d1a6056c17165d154b2a03b56145a5f12478
Volker Theile <vtheile@suse.com>
2021-02-05 08:46:59 +0100

    glass: Improve install type page
    * Rename component to InstallModePage like in Harvester
    
    Signed-off-by: Volker Theile <vtheile@suse.com>
    Signed-off-by: Alexandra <asettle@suse.com>

---
+++ src/glass/src/app/pages/install-mode-page/install-mode-page.component.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { InstallModePageComponent } from '~/app/pages/install-mode-page/install-mode-page.component';
+import { PagesModule } from '~/app/pages/pages.module';
+
+describe('InstallModePageComponent', () => {
+  let component: InstallModePageComponent;
+  let fixture: ComponentFixture<InstallModePageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PagesModule, RouterTestingModule]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(InstallModePageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
