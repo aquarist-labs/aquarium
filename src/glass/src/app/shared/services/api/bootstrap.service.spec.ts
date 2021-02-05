86ab73b48884c88908e0f0ceaec34f8f8598dc26
Volker Theile <vtheile@suse.com>
2021-02-05 12:23:27 +0100

    glass: Implement services to access the backend API
    Fixes: https://github.com/aquarist-labs/aquarium/issues/57
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

---
+++ src/glass/src/app/shared/services/api/bootstrap.service.spec.ts
@@ -0,0 +1,33 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { BootstrapService } from './bootstrap.service';
+
+describe('BootstrapService', () => {
+  let service: BootstrapService;
+  let httpTesting: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(BootstrapService);
+    httpTesting = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should call start', () => {
+    service.start().subscribe();
+    const req = httpTesting.expectOne('api/bootstrap/start');
+    expect(req.request.method).toBe('POST');
+  });
+
+  it('should call status', () => {
+    service.status().subscribe();
+    const req = httpTesting.expectOne('api/bootstrap/status');
+    expect(req.request.method).toBe('GET');
+  });
+});
---
+++ src/glass/src/app/shared/services/api/bootstrap.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export type BootstrapBasicReply = {
+  success: boolean;
+};
+
+export type BootstrapStatusReply = {
+  status: 'none' | 'running' | 'done' | 'error';
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BootstrapService {
+  private url = 'api/bootstrap';
+
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Start the bootstrapping.
+   */
+  start(): Observable<BootstrapBasicReply> {
+    return this.http.post<BootstrapBasicReply>(`${this.url}/start`, null, {});
+  }
+
+  /**
+   * Get the bootstrap status.
+   */
+  status(): Observable<BootstrapStatusReply> {
+    return this.http.get<BootstrapStatusReply>(`${this.url}/status`);
+  }
+}
