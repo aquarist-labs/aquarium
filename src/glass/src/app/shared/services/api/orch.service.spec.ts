96f7cff73189b8974be122ad8759e7a21661fe18
Tatjana Dehler <tdehler@suse.com>
2021-02-11 18:45:42 +0100

    glass: add orch service
    Add orch service in order to call hosts and devices endpoints.
    
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

---
+++ src/glass/src/app/shared/services/api/orch.service.spec.ts
@@ -0,0 +1,33 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { OrchService } from './orch.service';
+
+describe('OrchService', () => {
+  let service: OrchService;
+  let httpTesting: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(OrchService);
+    httpTesting = TestBed.inject(HttpTestingController);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should call devices', () => {
+    service.devices().subscribe();
+    const req = httpTesting.expectOne('api/orch/devices');
+    expect(req.request.method).toBe('GET');
+  });
+
+  it('should call hosts', () => {
+    service.hosts().subscribe();
+    const req = httpTesting.expectOne('api/orch/hosts');
+    expect(req.request.method).toBe('GET');
+  });
+});
