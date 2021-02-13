b74c09b247f5dcef525602270fcd421c6c3fff8e
Joao Eduardo Luis <joao@suse.com>
2021-02-13 18:10:17 -0100

    gravel: ctrl/storage: gather & keep storage state
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

---
+++ tests/gravel/test_storage.py
@@ -0,0 +1,22 @@
+# project aquarium's backend
+# Copyright (C) 2021 SUSE, LLC.
+
+import asyncio
+from gravel.controllers.resources import storage
+from gravel.controllers.orch.ceph import Mon
+
+
+async def main():
+
+    await storage.tick()
+    print(await storage.usage())
+    print(storage.available)
+    print(storage.used)
+    print(storage.total)
+    assert storage.total == storage.used + storage.available
+
+    mon = Mon()
+    print(mon.df().json(indent=2))
+
+if __name__ == "__main__":
+    asyncio.run(main())
