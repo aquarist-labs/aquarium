aquarist-labs/aquarium

commit 9289ce3f3847be991582cde160d2fd2a65ae9d88

aquarium: fixup partial revert (e08060e5)
revert of a98714d9 missed the aquarium module

Signed-off-by: Michael Fritch <mfritch@suse.com>

src/aquarium.py
@@ -25,7 +25,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.logger import logger as fastapi_logger
 
-from gravel import gstate
+from gravel.controllers.gstate import GlobalState
 
 from gravel.api import bootstrap
 from gravel.api import orch
@@ -38,6 +38,8 @@ logger: logging.Logger = fastapi_logger
 app = FastAPI()
 api = FastAPI()
 
+gstate: GlobalState = GlobalState()
+
 
 @app.on_event("startup")  # type: ignore
 async def on_startup():
@@ -47,7 +49,7 @@ async def on_startup():
         uvilogger.setLevel(logging.DEBUG)
         logger.setLevel(logging.DEBUG)
     logger.info("Aquarium startup!")
-    
+
     # create a task simply so we don't hold up the startup
     asyncio.create_task(gstate.start())
     pass

commit 2b04da575e0f8a95af1c1031d347dc6fbbf8b60c

gravel: fix regression introduced by 9289ce3
Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/aquarium.py
@@ -25,7 +25,7 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.logger import logger as fastapi_logger
 
-from gravel.controllers.gstate import GlobalState
+from gravel.controllers.gstate import gstate
 
 from gravel.api import bootstrap
 from gravel.api import orch
@@ -37,8 +37,6 @@ logger: logging.Logger = fastapi_logger
 
 app = FastAPI()
 api = FastAPI()
-
-gstate: GlobalState = GlobalState()
 
 
 @app.on_event("startup")  # type: ignore