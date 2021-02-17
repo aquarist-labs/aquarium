204c4d7a3524b56566ab3925967983f60b4c3eff
Joao Eduardo Luis <joao@suse.com>
2021-02-17 19:39:54 -0100

    glass: support cluster health info on '/api/status'
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- src/glass/src/app/shared/services/api/status.service.ts
+++ src/glass/src/app/shared/services/api/status.service.ts
@@ -2,12 +2,33 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+export interface HealthCheckSummary {
+  message: string;
+  count: number;
+}
+
+export interface HealthStatus {
+  status: string;
+  checks: { [id: string]: HealthCheckSummary };
+}
+
+export interface ClusterStatus {
+  fsid: string;
+  /* eslint-disable @typescript-eslint/naming-convention */
+  election_epoch: number;
+  quorum: number[];
+  quorum_names: string[];
+  quorum_age: number;
+  health: HealthStatus;
+}
+
 export type Status = {
   /* eslint-disable @typescript-eslint/naming-convention */
   deployment_state: {
     last_modified: string;
     stage: 'none' | 'bootstrapping' | 'bootstrapped' | 'ready' | 'unknown';
   };
+  cluster?: ClusterStatus;
 };
 
 @Injectable({
