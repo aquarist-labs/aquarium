13d26f141a14e06add6c5296f0ba3365cf65c600
Volker Theile <vtheile@suse.com>
2021-02-19 12:05:00 +0100

    glass: Create a reusable dialog component
    Fixes: https://github.com/aquarist-labs/aquarium/issues/181
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/glass/src/app/shared/components/components.module.ts
+++ src/glass/src/app/shared/components/components.module.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MaterialModule } from '~/app/material.modules';
-
-import { SubmitButtonComponent } from './submit-button/submit-button.component';
+import { DialogComponent } from '~/app/shared/components/dialog/dialog.component';
+import { SubmitButtonComponent } from '~/app/shared/components/submit-button/submit-button.component';
 
 @NgModule({
-  declarations: [SubmitButtonComponent],
-  exports: [SubmitButtonComponent],
-  imports: [CommonModule, MaterialModule]
+  declarations: [SubmitButtonComponent, DialogComponent],
+  exports: [SubmitButtonComponent, DialogComponent],
+  imports: [CommonModule, FlexLayoutModule, MaterialModule]
 })
 export class ComponentsModule {}
--- src/glass/src/app/shared/components/submit-button/submit-button.component.ts
+++ src/glass/src/app/shared/components/submit-button/submit-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter,Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AbstractControl, FormGroup } from '@angular/forms';
 import * as _ from 'lodash';
 
