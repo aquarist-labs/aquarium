130e44e7de673ce6fcca0811d07e441c2efa34ef
Stephan Müller <smueller@suse.com>
2021-02-23 18:56:14 +0100

    glass: Show real data in capacity widget
    Fixes #210
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/glass/src/app/core/dashboard/widgets/capacity-dashboard-widget/capacity-dashboard-widget.component.spec.ts
+++ src/glass/src/app/core/dashboard/widgets/capacity-dashboard-widget/capacity-dashboard-widget.component.spec.ts
@@ -1,5 +1,7 @@
 /* eslint-disable max-len */
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DashboardModule } from '~/app/core/dashboard/dashboard.module';
 import { CapacityDashboardWidgetComponent } from '~/app/core/dashboard/widgets/capacity-dashboard-widget/capacity-dashboard-widget.component';
@@ -10,7 +12,7 @@ describe('CapacityDashboardWidgetComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [DashboardModule]
+      imports: [DashboardModule, HttpClientTestingModule, BrowserAnimationsModule]
     }).compileComponents();
   });
 
--- src/glass/src/app/pages/dashboard-page/dashboard-page.component.spec.ts
+++ src/glass/src/app/pages/dashboard-page/dashboard-page.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DashboardPageComponent } from '~/app/pages/dashboard-page/dashboard-page.component';
 import { PagesModule } from '~/app/pages/pages.module';
@@ -10,7 +11,7 @@ describe('DashboardPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, PagesModule]
+      imports: [HttpClientTestingModule, PagesModule, BrowserAnimationsModule]
     }).compileComponents();
   });
 
