4c7ad63bef69948b5ae93ec16f9e52ed7bcebf5a
Volker Theile <vtheile@suse.com>
2021-02-26 11:12:03 +0100

    glass: Add a host dashboard widget
    - Add hosts dashboard widget
    - Keep polling data in case of an error
    - Refactored response of /orch/hosts
    
    Fixes: https://github.com/aquarist-labs/aquarium/issues/202
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/glass/src/app/core/dashboard/dashboard.module.ts
+++ src/glass/src/app/core/dashboard/dashboard.module.ts
@@ -6,6 +6,7 @@ import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { CapacityDashboardWidgetComponent } from '~/app/core/dashboard/widgets/capacity-dashboard-widget/capacity-dashboard-widget.component';
 import { HealthDashboardWidgetComponent } from '~/app/core/dashboard/widgets/health-dashboard-widget/health-dashboard-widget.component';
+import { HostsDashboardWidgetComponent } from '~/app/core/dashboard/widgets/hosts-dashboard-widget/hosts-dashboard-widget.component';
 import { ServicesDashboardWidgetComponent } from '~/app/core/dashboard/widgets/services-dashboard-widget/services-dashboard-widget.component';
 import { SysInfoDashboardWidgetComponent } from '~/app/core/dashboard/widgets/sys-info-dashboard-widget/sys-info-dashboard-widget.component';
 import { VolumesDashboardWidgetComponent } from '~/app/core/dashboard/widgets/volumes-dashboard-widget/volumes-dashboard-widget.component';
@@ -18,14 +19,16 @@ import { SharedModule } from '~/app/shared/shared.module';
     VolumesDashboardWidgetComponent,
     HealthDashboardWidgetComponent,
     ServicesDashboardWidgetComponent,
-    SysInfoDashboardWidgetComponent
+    SysInfoDashboardWidgetComponent,
+    HostsDashboardWidgetComponent
   ],
   exports: [
     CapacityDashboardWidgetComponent,
     VolumesDashboardWidgetComponent,
     HealthDashboardWidgetComponent,
     ServicesDashboardWidgetComponent,
-    SysInfoDashboardWidgetComponent
+    SysInfoDashboardWidgetComponent,
+    HostsDashboardWidgetComponent
   ],
   imports: [CommonModule, FlexLayoutModule, MaterialModule, NgxChartsModule, SharedModule]
 })
