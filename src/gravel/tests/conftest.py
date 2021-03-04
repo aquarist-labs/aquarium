5304e6ba91dcda279f30c5ceac91e62b56e0893d
Joshua Hesketh <josh@nitrotech.org>
2021-03-04 15:38:27 +1100

    Use pytest fixtures for test_ceph
    This also moves assets into a data directory.
    
    Signed-off-by: Joshua Hesketh <josh@nitrotech.org>

--- src/gravel/tests/conftest.py
+++ src/gravel/tests/conftest.py
@@ -1,5 +1,10 @@
+import os
+import pytest
 import sys
 from unittest import mock
+
+
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def mock_ceph_modules():
@@ -20,3 +25,29 @@ def mock_ceph_modules():
 
 
 mock_ceph_modules()
+
+
+@pytest.fixture(params=['default_ceph.conf'])
+def ceph_conf_file_fs(request, fs):
+    """ This fixture uses pyfakefs to stub filesystem calls and return
+    any files created with the parent `fs` fixture. """
+    fs.add_real_file(
+        os.path.join(DATA_DIR, request.param),
+        target_path='/etc/ceph/ceph.conf'
+    )
+    yield fs
+
+
+@pytest.fixture()
+def get_data_contents(fs):
+    # For tests to be able to access any files in the DATA_DIR, we need to
+    # add them to the fake filesystem. (If you open any files before the
+    # fakefs is set up they will still be accessible, so this is only as a
+    # convenience in-case you've order the `fs` before any other fixtures).
+    fs.add_real_directory(DATA_DIR)
+
+    def _get_data_contents(fn):
+        with open(os.path.join(DATA_DIR, fn), 'r') as f:
+            contents = f.read()
+        return contents
+    yield _get_data_contents
--- src/gravel/tests/test_ceph.py
+++ src/gravel/tests/test_ceph.py
@@ -2,90 +2,81 @@
 # Copyright (C) 2021 SUSE, LLC.
 
 import json
+import os
 import pytest
-import unittest.mock
 
 from typing import Any, Dict
-from pyfakefs.fake_filesystem_unittest \
-    import TestCase  # pyright: reportMissingTypeStubs=false
 
 from gravel.controllers.orch.ceph import Mgr, Mon
-from .outputs import mon_df_raw, mon_osdmap_raw
 
-CEPH_CONF_FILE = '/etc/ceph/ceph.conf'
-CEPH_CONF_CONTENT = '''
-# minimal ceph.conf for 049e7b49-0c86-4373-85a4-cb9d50c374a7
-[global]
-        fsid = 049e7b49-0c86-4373-85a4-cb9d50c374a7
-        mon_host = [v2:192.168.1.1:40919/0,v1:192.168.1.1:40920/0] [v2:192.168.1.1:40921/0,v1:192.168.1.1:40922/0] [v2:192.168.1.1:40923/0,v1:192.168.1.1:40924/0]
-'''  # noqa:E501
+DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
-class TestCeph(TestCase):
+def test_ceph_conf(fs):
+    # default location
+    fs.add_real_file(
+        os.path.join(DATA_DIR, 'default_ceph.conf'),
+        target_path='/etc/ceph/ceph.conf'
+    )
+    Mgr()
+    Mon()
 
-    def setUp(self):
-        self.setUpPyfakefs()
-        self.fs.create_file(CEPH_CONF_FILE, contents=CEPH_CONF_CONTENT)
+    # custom location
+    conf_file = '/foo/bar/baz.conf'
+    fs.add_real_file(
+        os.path.join(DATA_DIR, 'default_ceph.conf'),
+        target_path=conf_file
+    )
+    Mgr(conf_file=conf_file)
+    Mon(conf_file=conf_file)
 
-    def test_ceph_conf(self):
-        # default location
-        Mgr()
-        Mon()
-
-        # custom location
-        conf_file = '/foo/bar/baz.conf'
-        self.fs.create_file(conf_file, contents=CEPH_CONF_CONTENT)
+    # invalid location
+    conf_file = "missing.conf"
+    with pytest.raises(FileNotFoundError, match=conf_file):
         Mgr(conf_file=conf_file)
         Mon(conf_file=conf_file)
 
-        # invalid location
-        conf_file = "missing.conf"
-        with pytest.raises(FileNotFoundError, match=conf_file):
-            Mgr(conf_file=conf_file)
-            Mon(conf_file=conf_file)
 
-    def call(self, raw_value: str) -> Any:
-        return json.loads(raw_value)
+def test_mon_df(ceph_conf_file_fs, mocker, get_data_contents):
+    mon = Mon()
+    mon.call = mocker.MagicMock(  # type: ignore
+        return_value=json.loads(get_data_contents('mon_df_raw.json')))
 
-    def test_mon_df(self):
+    res = mon.df()
+    assert res.stats.total_bytes == 0
 
-        mon = Mon()
-        mon.call = unittest.mock.MagicMock(  # type: ignore
-            return_value=self.call(mon_df_raw))
 
-        res = mon.df()
-        self.assertEqual(res.stats.total_bytes, 0)
+def test_get_osdmap(ceph_conf_file_fs, mocker, get_data_contents):
+    mon = Mon()
+    mon.call = mocker.MagicMock(  # type: ignore
+        return_value=json.loads(get_data_contents('mon_osdmap_raw.json'))
+    )
+    res = mon.get_osdmap()
+    assert res.epoch == 4
 
-    def test_get_osdmap(self):
-        mon = Mon()
-        mon.call = unittest.mock.MagicMock(  # type: ignore
-            return_value=self.call(mon_osdmap_raw)
-        )
-        res = mon.get_osdmap()
-        self.assertEqual(res.epoch, 4)
 
-    def test_get_pools(self):
-        mon = Mon()
-        mon.call = unittest.mock.MagicMock(  # type: ignore
-            return_value=self.call(mon_osdmap_raw)
-        )
-        res = mon.get_pools()
-        self.assertEqual(len(res), 0)
+def test_get_pools(ceph_conf_file_fs, mocker, get_data_contents):
+    mon = Mon()
+    mon.call = mocker.MagicMock(  # type: ignore
+        return_value=json.loads(get_data_contents('mon_osdmap_raw.json'))
+    )
+    res = mon.get_pools()
+    assert len(res) == 0
 
-    def test_set_pool_size(self):
 
-        def argscheck(cls: Any, args: Dict[str, Any]) -> Any:
-            self.assertIn("prefix", args)
-            self.assertIn("pool", args)
-            self.assertIn("var", args)
-            self.assertIn("val", args)
-            self.assertEqual(args["prefix"], "osd pool set")
-            self.assertEqual(args["pool"], "foobar")
-            self.assertEqual(args["var"], "size")
-            self.assertEqual(args["val"], "2")
+def test_set_pool_size(ceph_conf_file_fs, mocker):
+    def argscheck(cls: Any, args: Dict[str, Any]) -> Any:
+        assert "prefix" in args
+        assert "pool" in args
+        assert "var" in args
+        assert "val" in args
+        assert args["prefix"] == "osd pool set"
+        assert args["pool"] == "foobar"
+        assert args["var"] == "size"
+        assert args["val"] == "2"
 
-        with unittest.mock.patch.object(
-            Mon, "call", new=argscheck  # type:ignore
-        ):
-            mon = Mon()
-            mon.set_pool_size("foobar", 2)
+    mocker.patch.object(
+        Mon, "call", new=argscheck  # type:ignore
+    )
+    mon = Mon()
+    mon.set_pool_size("foobar", 2)
