810fb6919bc8df9586a3ef4c0a38266814062a03
Volker Theile <vtheile@suse.com>
2021-03-05 14:09:13 +0100

    glass: Improve i18n implementation
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/glass/src/app/core/top-bar/top-bar.component.ts
+++ src/glass/src/app/core/top-bar/top-bar.component.ts
@@ -1,37 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 
-import { LocalStorageService } from '~/app/shared/services/local-storage.service';
+import { getCurrentLanguage, setCurrentLanguage, supportedLanguages } from '~/app/i18n.helper';
 
 @Component({
   selector: 'glass-top-bar',
   templateUrl: './top-bar.component.html',
   styleUrls: ['./top-bar.component.scss']
 })
-export class TopBarComponent implements OnInit {
+export class TopBarComponent {
   // eslint-disable-next-line @angular-eslint/no-input-rename
   @Input('navigationSidenav')
   navigationSidenav!: MatSidenav;
 
-  languages = {
-    /* eslint-disable @typescript-eslint/naming-convention */
-    en_GB: 'English',
-    de_DE: 'Deutsch'
-  };
+  languages = supportedLanguages;
   currentLanguage: string;
 
-  constructor(private localStorageService: LocalStorageService) {
-    this.currentLanguage = this.localStorageService.get('language', 'en_GB') as string;
+  constructor() {
+    this.currentLanguage = getCurrentLanguage();
   }
-
-  ngOnInit(): void {}
 
   onToggleNavigationBar() {
     this.navigationSidenav.toggle();
   }
 
   onSelectLanguage(language: string): void {
-    this.localStorageService.set('language', language);
+    setCurrentLanguage(language);
     document.location.replace('');
   }
 }
