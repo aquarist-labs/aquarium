80bb7854520cf81220083ac8198e86dab838be71
Joao Eduardo Luis <joao@suse.com>
2021-03-12 07:23:22 -0100

    gravel: tests: add type hints to some units
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- src/gravel/tests/conftest.py
+++ src/gravel/tests/conftest.py
@@ -1,15 +1,31 @@
+# project aquarium's backend
+# Copyright (C) 2021 SUSE, LLC.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import os
+from typing import Any, Generator, Optional
 import pytest
 import sys
 from unittest import mock
-
+from pyfakefs import fake_filesystem  # pyright: reportMissingTypeStubs=false
+from _pytest.fixtures import SubRequest
+from pytest_mock import MockerFixture
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 
 
 def mock_ceph_modules():
     class MockRadosError(Exception):
-        def __init__(self, message, errno=None):
+        def __init__(self, message: str, errno: Optional[int] = None):
             super(MockRadosError, self).__init__(message)
             self.errno = errno
 
@@ -28,19 +44,25 @@ mock_ceph_modules()
 
 
 @pytest.fixture(params=['default_ceph.conf'])
-def ceph_conf_file_fs(request, fs):
+def ceph_conf_file_fs(
+    request: SubRequest,
+    fs: fake_filesystem.FakeFilesystem
+):
     """ This fixture uses pyfakefs to stub filesystem calls and return
     any files created with the parent `fs` fixture. """
-    fs.add_real_file(
-        os.path.join(DATA_DIR, request.param),
+    fs.add_real_file(  # pyright: reportUnknownMemberType=false
+        os.path.join(
+            DATA_DIR,
+            request.param  # pyright: reportUnknownArgumentType=false
+        ),
         target_path='/etc/ceph/ceph.conf'
     )
     yield fs
 
 
 @pytest.fixture()
-def get_data_contents(fs):
-    def _get_data_contents(dir, fn):
+def get_data_contents(fs: fake_filesystem.FakeFilesystem):
+    def _get_data_contents(dir: str, fn: str):
         # For tests to be able to access any file we need to  add them to the
         # fake filesystem. (If you open any files before the fakefs is set up
         # they will still be accessible, so this is only as a convenience
@@ -57,14 +79,14 @@ def get_data_contents(fs):
 
 
 @pytest.fixture()
-def gstate(mocker):
+def gstate(mocker: MockerFixture):
     mocker.patch('gravel.controllers.config.Config')
     from gravel.controllers.gstate import gstate as _gstate
     yield _gstate
 
 
 @pytest.fixture()
-def services(gstate, mocker):
+def services(gstate: Generator[Any, None, None], mocker: MockerFixture):
     class MockStorage(mocker.MagicMock):  # type: ignore
         available = 2000
     mocker.patch('gravel.controllers.resources.storage', MockStorage)
