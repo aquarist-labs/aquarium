fcda1539193a0ed95cb41896c5ca253f59cd4a9e
Joshua Hesketh <josh@nitrotech.org>
2021-04-28 19:55:47 +1000

    aqr: Use a factory for constructing our ASGI app
    This allows us to import the aquarium module without creating
    module level globals.
    
    Signed-off-by: Joshua Hesketh <josh@nitrotech.org>

--- src/aquarium.py
+++ src/aquarium.py
@@ -19,6 +19,7 @@ import os
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.logger import logger as fastapi_logger
+import uvicorn
 
 from gravel.controllers.gstate import GlobalState, setup_logging
 from gravel.controllers.nodes.mgr import NodeMgr
@@ -40,122 +41,120 @@ from gravel.api import (
 )
 
 
-logger: logging.Logger = fastapi_logger
+def app_factory():
+    logger: logging.Logger = fastapi_logger
 
+    api_tags_metadata = [
+        {
+            "name": "local",
+            "description": "Operations local to the node where the endpoint is being invoked."
+        },
+        {
+            "name": "bootstrap",
+            "description": "Allows creating a minimal cluster on the node."
+        },
+        {
+            "name": "orch",
+            "description": "Operations related to Ceph cluster orchestration."
+        },
+        {
+            "name": "status",
+            "description": "Allows obtaining operation status information."
+        },
+        {
+            "name": "services",
+            "description": "Create, destroy, and operate Aquarium Services."
+        },
+        {
+            "name": "nodes",
+            "description": "Perform Aquarium Cluster node operations"
+        },
+        {
+            "name": "devices",
+            "description": "Obtain and perform operations on cluster devices"
+        }
+    ]
 
-api_tags_metadata = [
-    {
-        "name": "local",
-        "description": "Operations local to the node where the endpoint is being invoked."
-    },
-    {
-        "name": "bootstrap",
-        "description": "Allows creating a minimal cluster on the node."
-    },
-    {
-        "name": "orch",
-        "description": "Operations related to Ceph cluster orchestration."
-    },
-    {
-        "name": "status",
-        "description": "Allows obtaining operation status information."
-    },
-    {
-        "name": "services",
-        "description": "Create, destroy, and operate Aquarium Services."
-    },
-    {
-        "name": "nodes",
-        "description": "Perform Aquarium Cluster node operations"
-    },
-    {
-        "name": "devices",
-        "description": "Obtain and perform operations on cluster devices"
-    }
-]
+    aquarium_app = FastAPI(docs_url=None)
+    aquarium_api = FastAPI(
+        title="Project Aquarium",
+        description="Project Aquarium is a SUSE-sponsored open source project " +
+                    "aiming at becoming an easy to use, rock solid storage " +
+                    "appliance based on Ceph.",
+        version="1.0.0",
+        openapi_tags=api_tags_metadata)
 
+    @aquarium_app.on_event("startup")  # type: ignore
+    async def on_startup():
 
-app = FastAPI(docs_url=None)
-api = FastAPI(
-    title="Project Aquarium",
-    description="Project Aquarium is a SUSE-sponsored open source project " +
-                "aiming at becoming an easy to use, rock solid storage " +
-                "appliance based on Ceph.",
-    version="1.0.0",
-    openapi_tags=api_tags_metadata)
+        lvl = "INFO" if not os.getenv("AQUARIUM_DEBUG") else "DEBUG"
+        setup_logging(lvl)
+        logger.info("Aquarium startup!")
 
+        gstate: GlobalState = GlobalState()
 
-@app.on_event("startup")  # type: ignore
-async def on_startup():
+        # init node mgr
+        logger.info("starting node manager")
+        nodemgr: NodeMgr = NodeMgr(gstate)
 
-    lvl = "INFO" if not os.getenv("AQUARIUM_DEBUG") else "DEBUG"
-    setup_logging(lvl)
-    logger.info("Aquarium startup!")
+        devices: Devices = Devices(gstate.config.options.devices.probe_interval, nodemgr)
+        gstate.add_devices(devices)
 
-    gstate: GlobalState = GlobalState()
+        status: Status = Status(gstate.config.options.status.probe_interval, gstate, nodemgr)
+        gstate.add_status(status)
 
-    # init node mgr
-    logger.info("starting node manager")
-    nodemgr: NodeMgr = NodeMgr(gstate)
+        inventory: Inventory = Inventory(
+            gstate.config.options.inventory.probe_interval,
+            nodemgr
+        )
+        gstate.add_inventory(inventory)
 
-    devices: Devices = Devices(gstate.config.options.devices.probe_interval, nodemgr)
-    gstate.add_devices(devices)
+        storage: Storage = Storage(gstate.config.options.storage.probe_interval, nodemgr)
+        gstate.add_storage(storage)
 
-    status: Status = Status(gstate.config.options.status.probe_interval, gstate, nodemgr)
-    gstate.add_status(status)
+        services: Services = Services(gstate.config.options.services.probe_interval, gstate, nodemgr)
+        gstate.add_services(services)
 
-    inventory: Inventory = Inventory(
-        gstate.config.options.inventory.probe_interval,
-        nodemgr
+        await nodemgr.start()
+        await gstate.start()
+
+        # Add instances into FastAPI's state:
+        aquarium_api.state.gstate = gstate
+        aquarium_api.state.nodemgr = nodemgr
+
+    @aquarium_app.on_event("shutdown")  # type: ignore
+    async def on_shutdown():
+        logger.info("Aquarium shutdown!")
+        await aquarium_api.state.gstate.shutdown()
+        logger.info("shutting down node manager")
+        await aquarium_api.state.nodemgr.shutdown()
+
+    aquarium_api.include_router(local.router)
+    aquarium_api.include_router(bootstrap.router)
+    aquarium_api.include_router(orch.router)
+    aquarium_api.include_router(status.router)
+    aquarium_api.include_router(services.router)
+    aquarium_api.include_router(nodes.router)
+    aquarium_api.include_router(devices.router)
+    aquarium_api.include_router(nfs.router)
+
+    #
+    # mounts
+    #   these should be the last things to be done, so fastapi is aware of
+    #   everything that comes before.
+    #
+    # api calls go here.
+    aquarium_app.mount(
+        "/api",
+        aquarium_api,
+        name="api"
     )
-    gstate.add_inventory(inventory)
+    # mounting root "/" must be the last thing, so it does not override "/api".
+    aquarium_app.mount(
+        "/",
+        StaticFiles(directory="./glass/dist/", html=True),
+        name="static"
+    )
 
-    storage: Storage = Storage(gstate.config.options.storage.probe_interval, nodemgr)
-    gstate.add_storage(storage)
+    return aquarium_app
 
-    services: Services = Services(gstate.config.options.services.probe_interval, gstate, nodemgr)
-    gstate.add_services(services)
-
-    await nodemgr.start()
-    await gstate.start()
-
-    # Add instances into FastAPI's state:
-    api.state.gstate = gstate
-    api.state.nodemgr = nodemgr
-
-
-@app.on_event("shutdown")  # type: ignore
-async def on_shutdown():
-    logger.info("Aquarium shutdown!")
-    await api.state.gstate.shutdown()
-    logger.info("shutting down node manager")
-    await api.state.nodemgr.shutdown()
-
-
-api.include_router(local.router)
-api.include_router(bootstrap.router)
-api.include_router(orch.router)
-api.include_router(status.router)
-api.include_router(services.router)
-api.include_router(nodes.router)
-api.include_router(devices.router)
-api.include_router(nfs.router)
-
-
-#
-# mounts
-#   these should be the last things to be done, so fastapi is aware of
-#   everything that comes before.
-#
-# api calls go here.
-app.mount(
-    "/api",
-    api,
-    name="api"
-)
-# mounting root "/" must be the last thing, so it does not override "/api".
-app.mount(
-    "/",
-    StaticFiles(directory="./glass/dist/", html=True),
-    name="static"
-)
