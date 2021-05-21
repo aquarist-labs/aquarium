0a5b38f87fafe58202fdb27c1e6f86cb76440418
Joao Eduardo Luis <joao@suse.com>
2021-05-21 12:32:58 +0000

    glass: services: obtain local node inventory
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

---
+++ src/glass/src/app/shared/services/api/local-inventory.service.ts
@@ -0,0 +1,83 @@
+/*
+ * Project Aquarium's frontend (glass)
+ * Copyright (C) 2021 SUSE, LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+import { Injectable } from '@angular/core';
+import { Observable, ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import {
+  Inventory,
+  LocalNodeService,
+  Nic,
+  NodeStatus,
+  Volume
+} from '~/app/shared/services/api/local.service';
+import { PollService } from '~/app/shared/services/poll.service';
+
+export type NICEntry = {
+  name: string;
+  info: Nic;
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalInventoryService {
+  public inventory$: Observable<Inventory>;
+
+  private inventorySource: ReplaySubject<Inventory> = new ReplaySubject<Inventory>(1);
+
+  constructor(private localService: LocalNodeService, private pollService: PollService) {
+    this.localService
+      .status()
+      .pipe(this.pollService.poll((status: NodeStatus) => !status.inited))
+      .subscribe({
+        next: (status: NodeStatus) => {
+          console.log('checking node status: ', status);
+          if (status.inited) {
+            this.obtainInventory();
+          }
+        }
+      });
+
+    this.inventory$ = this.inventorySource.asObservable();
+  }
+
+  getDevices(): Observable<Volume[]> {
+    return this.inventory$.pipe(map((inventory: Inventory) => inventory.disks));
+  }
+
+  getNICs(): Observable<NICEntry[]> {
+    return this.inventory$.pipe(
+      map((inventory: Inventory) => {
+        const entries: NICEntry[] = [];
+        Object.keys(inventory.nics).forEach((key: string) => {
+          entries.push({
+            name: key,
+            info: inventory.nics[key]
+          });
+        });
+        return entries;
+      })
+    );
+  }
+
+  private obtainInventory() {
+    this.localService.inventory().subscribe({
+      next: (inventory: Inventory) => {
+        this.inventorySource.next(inventory);
+      }
+    });
+  }
+}
