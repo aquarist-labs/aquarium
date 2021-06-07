501eb3ee93a107166efa3a774fab2659bfd8b17d
Volker Theile <vtheile@suse.com>
2021-06-07 10:46:52 +0200

    glass/gravel: expose ceph-volume device availability
    Fixes: https://github.com/aquarist-labs/aquarium/issues/475
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/glass/src/app/material.modules.ts
+++ src/glass/src/app/material.modules.ts
@@ -14,6 +14,7 @@
  */
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatRippleModule } from '@angular/material/core';
@@ -60,7 +61,8 @@ import { DomSanitizer } from '@angular/platform-browser';
     MatPaginatorModule,
     MatRadioModule,
     MatRippleModule,
-    MatSelectModule
+    MatSelectModule,
+    MatBadgeModule
   ]
 })
 export class MaterialModule {
