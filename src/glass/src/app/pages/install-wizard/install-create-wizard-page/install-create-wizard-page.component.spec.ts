abd13103aead0404d85c0baf9f589bc9d3a0ddf4
Volker Theile <vtheile@suse.com>
2021-06-08 10:06:44 +0200

    glass: Display error notifications only once
    In the install wizard error notifications are displayed twice, the first notification is shown automatically by the HTTP error interceptor and the second one by the install wizard. To prevent the duplicate notification 'preventDefault' must be called.
    
    Additionally a HTML linter error has been fixed.
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/glass/src/app/pages/install-wizard/install-create-wizard-page/install-create-wizard-page.component.spec.ts
+++ src/glass/src/app/pages/install-wizard/install-create-wizard-page/install-create-wizard-page.component.spec.ts
@@ -1,17 +1,21 @@
 /* eslint-disable max-len */
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { TranslateModule } from '@ngx-translate/core';
-import { ToastrModule } from 'ngx-toastr';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
 
 import { InstallCreateWizardPageComponent } from '~/app/pages/install-wizard/install-create-wizard-page/install-create-wizard-page.component';
 import { PagesModule } from '~/app/pages/pages.module';
+import { HttpErrorInterceptorService } from '~/app/shared/services/http-error-interceptor.service';
 
 describe('InstallCreateWizardPageComponent', () => {
   let component: InstallCreateWizardPageComponent;
   let fixture: ComponentFixture<InstallCreateWizardPageComponent>;
+  let httpTesting: HttpTestingController;
+  let toastrService: ToastrService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,6 +26,13 @@ describe('InstallCreateWizardPageComponent', () => {
         RouterTestingModule,
         ToastrModule.forRoot(),
         TranslateModule.forRoot()
+      ],
+      providers: [
+        {
+          provide: HTTP_INTERCEPTORS,
+          useClass: HttpErrorInterceptorService,
+          multi: true
+        }
       ]
     }).compileComponents();
   });
@@ -29,10 +40,47 @@ describe('InstallCreateWizardPageComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InstallCreateWizardPageComponent);
     component = fixture.componentInstance;
+    httpTesting = TestBed.inject(HttpTestingController);
+    toastrService = TestBed.inject(ToastrService);
+    spyOn(toastrService, 'error');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show notification only once [1]', fakeAsync(() => {
+    httpTesting
+      .expectOne({ url: 'api/nodes/deployment/status', method: 'GET' })
+      .error(new ErrorEvent('Unknown error'), { status: 500 });
+    tick(5);
+    expect(toastrService.error).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should show notification only once [2]', fakeAsync(() => {
+    httpTesting
+      .expectOne({ url: 'api/local/status', method: 'GET' })
+      .error(new ErrorEvent('Unknown error'), { status: 500 });
+    tick(5);
+    expect(toastrService.error).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should show notification only once [3]', fakeAsync(() => {
+    component.startBootstrap();
+    httpTesting
+      .match({ url: 'api/orch/hostname', method: 'PUT' })[0]
+      .error(new ErrorEvent('Unknown error'), { status: 500 });
+    tick(5);
+    expect(toastrService.error).toHaveBeenCalledTimes(1);
+  }));
+
+  it('should show notification only once [4]', fakeAsync(() => {
+    component.finishDeployment();
+    httpTesting
+      .match('api/nodes/deployment/finished')[0]
+      .error(new ErrorEvent('Unknown error'), { status: 500 });
+    tick(5);
+    expect(toastrService.error).toHaveBeenCalledTimes(1);
+  }));
 });
