c35854bd90a2bbb69e70bc374733eea84a09e703
Volker Theile <vtheile@suse.com>
2021-07-16 09:50:00 +0200

    [tools] Add authentication support to smoke tests
    - Add authenticate support to smoke tests
    - Add option to aqrtest.py to skip adding the Vagrant image
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- tools/libaqr/http.py
+++ tools/libaqr/http.py
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-from typing import Any
+from typing import Any, Optional
 import aiohttp
 from contextlib import asynccontextmanager
 
@@ -20,11 +20,13 @@ class HTTPSession:
     _session: aiohttp.ClientSession
     _port: int
     _url: str
+    _access_token: Optional[str]
 
     def __init__(self, session: aiohttp.ClientSession, port: int) -> None:
         self._session = session
         self._port = port
         self._url = f"http://127.0.0.1:{self._port}"
+        self._access_token = None
 
     async def get(
         self,
@@ -34,7 +36,10 @@ class HTTPSession:
     ) -> aiohttp.ClientResponse:
         sep = "" if what.startswith("/") else "/"
         endpoint = f"{self._url}{sep}{what}"
-        return await self._session.get(endpoint, *args, **kwargs)
+        headers = {}
+        self._update_headers(headers)
+        return await self._session.get(endpoint, headers=headers,
+                                       *args, **kwargs)
 
     async def post(
         self,
@@ -44,16 +49,50 @@ class HTTPSession:
     ) -> aiohttp.ClientResponse:
         sep = "" if what.startswith("/") else "/"
         endpoint = f"{self._url}{sep}{what}"
-        return await self._session.post(endpoint, *args, **kwargs)
+        headers = {}
+        self._update_headers(headers)
+        return await self._session.post(endpoint, headers=headers,
+                                        *args, **kwargs)
+
+    async def login(self, username: str, password: str):
+        print(f">>> Logging in (username={username}, password={password}) ...")
+        try:
+            auth = aiohttp.BasicAuth(username, password)
+            form_data = aiohttp.FormData()
+            form_data.add_field("username", username)
+            form_data.add_field("password", password)
+            form_data.add_field("grant_type", "password")
+            res = await self._session.post(f"{self._url}/auth/login",
+                                           auth=auth,
+                                           data=form_data)
+            assert res.status == 200
+            self._access_token = res.json()["access_token"]
+        except Exception as e:
+            print(f">>> Failed to log in")
+            raise e
+
+    async def logout(self):
+        print(">>> Logging out ...")
+        res = await self.post("/auth/logout")
+        assert res.status == 200
+        self._access_token = None
+
+    def _update_headers(self, headers):
+        if self._access_token:
+            headers["Authorization"] = f"Bearer {self._access_token}"
 
 
 @asynccontextmanager
-async def conn(port: int = 1337):
+async def conn(port: int = 1337, login=False, username="admin", password="aquarium"):
     clientsession = None
     try:
         clientsession = aiohttp.ClientSession()
         httpsession = HTTPSession(clientsession, port)
+        if login:
+            await httpsession.login(username, password)
         yield httpsession
     finally:
+        if httpsession and login:
+            await httpsession.logout()
         if clientsession:
             await clientsession.close()
