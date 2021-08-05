aquarist-labs/aquarium

commit bd3d76e19b49cc244829f7e88ae63828d55ad3a0

Adding virtualbox support, replace type with provider info
Signed-off-by: Alex Lau (AvengerMoJo) <alau@suse.com>

tools/libaqr/deployment.py
@@ -79,6 +79,7 @@ class Deployment:
         cls,
         name: str,
         box: str,
+        provider: str,
         num_nodes: int,
         num_disks: int,
         num_nics: int,
@@ -93,8 +94,9 @@ class Deployment:
         if path.exists():
             raise DeploymentExistsError()
 
-        if box not in vagrant.Vagrant.box_list():
-            raise BoxDoesNotExistError(box)
+        if (box, provider) not in vagrant.Vagrant.box_list():
+            logger.debug(f"boxlist: '{vagrant.Vagrant.box_list()}'")
+            raise BoxDoesNotExistError(box, provider)
 
         try:
             logger.debug(f"creating deployment '{name}'")
@@ -104,7 +106,7 @@ class Deployment:
             raise AqrError(f"error creating deployment '{name}': {str(e)}")
 
         vagrantfile_text = _gen_vagrantfile(
-            box, mount_path,
+            box, provider, mount_path,
             num_nodes, num_disks, num_nics
         )
         vagrantfile = path.joinpath("Vagrantfile")
@@ -201,6 +203,7 @@ class Deployment:
 
 def _gen_vagrantfile(
     boxname: str,
+    provider: str,
     rootdir: Optional[Path],
     nodes: int,
     disks: int,
@@ -211,18 +214,40 @@ def _gen_vagrantfile(
         f"generate template: nodes={nodes}, disks={disks}, nics={nics}"
     )
 
-    without_nfs_str: str = "true" if not rootdir else "false"
+    without_shared_folder_str: str = "true" if not rootdir else "false"
     rootdir = rootdir if rootdir else Path(".")
+
+    """ template for node storage, libvirt and virtualbox use different format """
+    create_node_storage: str = ""
+    if provider == "virtualbox":
+        create_node_storage = \
+                f"""
+vb_config = `VBoxManage list systemproperties`.split(/\\n/).grep(/Default machine folder/).first
+diskpath = vb_config.split(':', 2)[1].strip() """
+        for nid in range(1, nodes+1):
+            for did in range(1, disks+1):
+                create_node_storage += \
+                        f"""
+N{nid}DISK{did} = File.join(diskpath, '{boxname}-node{nid}', 'node{nid}-disk{did}.vdi') """
+
 
     template: str = \
         f"""
 # -*- mode: ruby -*-
 # vim: set ft=ruby
+{create_node_storage}
 
 Vagrant.configure("2") do |config|
     config.vm.box = "{boxname}"
     config.vm.synced_folder ".", "/vagrant", disabled: true
-    config.vm.synced_folder "{rootdir}", "/srv/aquarium", type: "nfs", nfs_udp: false, disabled: {without_nfs_str}
+
+    config.vm.provider :virtualbox do |_, override|
+        override.vm.synced_folder "{rootdir}", "/srv/aquarium", type: "virtualbox", disabled: {without_shared_folder_str}
+    end
+    config.vm.provider :libvirt do |_, override|
+        override.vm.synced_folder "{rootdir}", "/srv/aquarium", type: "nfs", nfs_udp: false, disabled: {without_shared_folder_str}
+    end
+
     config.vm.guest = "suse"
 
         """
@@ -238,7 +263,25 @@ Vagrant.configure("2") do |config|
                 """
 
         node_storage: str = ""
-        for _ in range(disks):
+        if provider == "virtualbox":
+            node_storage += \
+                    f"""
+            lv.customize ['storagectl', :id, '--name', 'SATA Controller', '--portcount', '6'] """
+
+        for did in range(1, disks+1):
+            if provider == "libvirt":
+                serial = "".join(random.choice("0123456789") for _ in range(8))
+                node_storage += \
+                        f"""
+            lv.storage :file, size: "8G", type: "qcow2", serial: "{serial}" """
+            elif provider == "virtualbox":
+                node_storage += \
+                        f"""
+            unless File.exist?(N{nid}DISK{did})
+                lv.customize ['createhd', '--filename', N{nid}DISK{did}, '--format', 'VDI', '--size', '20000' ]
+            end
+            lv.customize ['storageattach', :id,  '--storagectl', 'SATA Controller', '--port', {did}, '--device', 0, '--type', 'hdd', '--medium', N{nid}DISK{did}] """
+
             serial = "".join(random.choice("0123456789") for _ in range(8))
             node_storage += \
                 f"""
@@ -250,7 +293,7 @@ Vagrant.configure("2") do |config|
             f"""
     config.vm.define :"node{nid}", primary: {is_primary_str} do |node|
         node.vm.hostname = "node{nid}"
-        node.vm.network "forwarded_port", guest: 1337, host: {host_port}, host_ip: "*"
+        node.vm.network "forwarded_port", guest: 1337, host: {host_port}
         {node_networks}
 
         node.vm.provider "libvirt" do |lv|

tools/aqrdev
@@ -188,7 +188,7 @@ def cmd_create(
     avail_boxes = Vagrant.box_list()
     avail_deployments: List[Deployment] = []
     try:
-        avail_images = [e.name for e in Image.list(find_builds_path())]
+        avail_images = Image.list(find_builds_path())
         avail_deployments = [v for v in ctx.deployments.values()]
     except RootNotFoundError:
         click.secho("Unable to find git repository's root dir")
@@ -200,26 +200,52 @@ def cmd_create(
         click.secho(f"Deployment '{name}' already exists", fg="red")
         sys.exit(errno.EEXIST)
 
+    """
+    User did not assign box or image
+    1) search for existing default aquarium box and provider type, then flag it
+    2) look up available image name and provider type, then flag it 
+    3) if nither exist , error out
+    """
     if not box and not image:
-        if "aquarium" in avail_boxes:
-            box = "aquarium"
-        elif avail_images and "aquarium" in avail_images:
-            image = "aquarium"
-        elif name in avail_boxes:
-            box = name
-        elif avail_images and name in avail_images:
-            image = name
+        click.secho(f"Using default 'aquarium' as box name and image name", fg="green")
+        aquarium_box_exist = False
+        aquarium_image_exist = False
+        for box_name, provider_name in avail_boxes:
+            if box_name == "aquarium" or box_name == name:
+                aquarium_box_exist = True
+                box = box_name
+                provider = provider_name
+        for img_obj in avail_images:
+            if aquarium_box_exist:
+                if img_obj.name == box and img_obj.provider == provider:
+                    aquarium_image_exist = True
+            elif img_obj.name == "aquarium" or img_obj.name == name and img_obj.provider == provider:
+                aquarium_image_exist = True
+                image = img_obj.name
+                provider = img_obj.provider
+
+        if aquarium_box_exist:
+            click.secho(f"Box '{box}' exist with Provider '{provider}' is availble", fg="green")
+        elif aquarium_image_exist:
+            click.secho(f"Image '{image}' exist with Provider '{provider}' is availble", fg="green")
         else:
-            click.secho("Unable to find default box 'aquarium'", fg="red")
+            click.secho("Unable to find default box or default image 'aquarium'", fg="red")
             sys.exit(errno.ENOENT)
 
+    """
+    Either User assign the image name or we get it from listing existing image
+    1) check if user assign image still exist
+    2) if image name is box name with force flag, remove the box
+    3) create box with image name, if image already in box list with no force flag, reuse
+    """
     if not box and image:
+        click.secho(f"Using image '{image}' to create box and deployment", fg="green")
 
-        if avail_images and image not in avail_images:
+        if image not in [img_obj.name for img_obj in avail_images]:
             click.secho(f"Image '{image}' not found", fg="red")
             sys.exit(errno.ENOENT)
 
-        if image in avail_boxes and force:
+        if image in [image_name for image_name, _ in avail_boxes] and force:
             # remove existing box first
             try:
                 Vagrant.box_remove(image)
@@ -237,8 +263,8 @@ def cmd_create(
             click.secho(f"Importing image '{image}' as Vagrant box", fg="cyan")
             builds_path = ctx.builds_path
 
-            Image.add(builds_path, image)
-            avail_boxes.append(image)
+            imgobj = Image.add(builds_path, image)
+            avail_boxes = Vagrant.box_list()
             box = image
             click.secho(
                 f"Imported image '{image}' as Vagrant box",
@@ -256,12 +282,24 @@ def cmd_create(
         except BuildsPathNotFoundError:
             click.secho("Unable to find builds path", fg="red")
             sys.exit(errno.ENOENT)
+        for image_name, provider_name in avail_boxes:
+            if image_name == image:
+                box = image
+                provider = provider_name
+        click.secho(f"Imported image: '{box}' as Vagrant box Provider: {provider}", fg="green")
     elif box and image:
         click.secho("Please provide only one of '--box' or '--image'", fg="red")
         sys.exit(errno.EINVAL)
 
     assert box is not None
-    if box not in avail_boxes:
+    box_exist = False
+    for box_name, provider_name in avail_boxes:
+        if box_name == box:
+            provider = provider_name
+            box_exist = True
+    if box_exist:
+        click.secho(f"Using box '{box}' provider '{provider}'", fg="green")
+    else:
         click.secho(f"Unable to find box '{box}'", fg="red")
         return
 
@@ -272,7 +310,7 @@ def cmd_create(
         assert deployment_path.exists()
 
         Deployment.create(
-            name, box, num_nodes, num_disks, num_nics,
+            name, box, provider, num_nodes, num_disks, num_nics,
             deployment_path, find_root()
         )
     except DeploymentPathNotFoundError:
@@ -543,12 +581,13 @@ def cmd_box_list(ctx: AppCtx, verbose: bool) -> None:
         return
 
     headerchr = chr(0x25cf)
-    for boxname in boxes:
+    for boxname, provider in boxes:
         num_deployments = 0 if boxname not in deployment_per_box \
                             else len(deployment_per_box[boxname])
-        click.secho("{} {} ({} deployments)".format(
+        click.secho("{} {} ({}) ({} deployments)".format(
             click.style(headerchr, fg="cyan", bold=True),
             click.style(boxname, fg="white", bold=True),
+            click.style(provider, fg="white", bold=False),
             num_deployments
         ))
 
@@ -592,13 +631,13 @@ def cmd_images_list(ctx: AppCtx) -> None:
         lst.append({
             "name": entry.name,
             "path": str(entry.path),
-            "type": entry.type
+            "provider": entry.provider
         })
         if not ctx.json:
             print("{} {} ({})".format(
                 click.style("*", fg="cyan"),
                 click.style(entry.name, fg="white", bold=True),
-                click.style(entry.type, fg="white")
+                click.style(entry.provider, fg="white")
             ))
     if ctx.json:
         print(json.dumps(lst))

tools/libaqr/images.py
@@ -27,12 +27,12 @@ from libaqr.errors import (
 class Image:
     _name: str
     _path: Path
-    _type: str
+    _provider: str
 
-    def __init__(self, name: str, path: Path, type: str) -> None:
+    def __init__(self, name: str, path: Path, provider: str) -> None:
         self._name = name
         self._path = path
-        self._type = type
+        self._provider = provider
         pass
 
     @classmethod
@@ -49,13 +49,13 @@ class Image:
             path = buildspath.joinpath(build)
             assert path.exists()
             try:
-                type, imgpath = cls._get_vagrant_image(path)
+                provider, imgpath = cls._get_vagrant_image(path)
             except FileNotFoundError:
                 continue
-            assert type
+            assert provider
             assert imgpath
 
-            images.append(Image(build, imgpath, type))
+            images.append(Image(build, imgpath, provider))
 
         return images
 
@@ -79,7 +79,7 @@ class Image:
         assert imgpath.exists()
 
         try:
-            Vagrant.box_add(name, imgpath)
+            Vagrant.box_add(name, imgpath, imgtype)
         except BoxAlreadyExistsError as e:
             raise e  # just being explicit about what we might propagate
         except VagrantError as e:
@@ -90,17 +90,17 @@ class Image:
     @classmethod
     def _get_vagrant_image(cls, path: Path) -> Tuple[str, Path]:
         """
-        Return vagrant image type and path if present below the `path`,
+        Return vagrant image provider and path if present below the `path`,
         otherwise raises FileNotFoundError.
 
-        Possible values for image type: 'unknown', 'libvirt'.
+        Possible values for image provider: 'unknown', 'libvirt', 'virtualbox'.
         """
         assert path.exists()
         outdir = path.joinpath("_out")
         if not outdir.exists():
             raise FileNotFoundError()
 
-        _type = 'unknown'
+        _provider = 'unknown'
         # we are searching for vagrant box image file
         _name = next((_ for _ in next(os.walk(outdir))[2]
                                 if _.startswith('project-aquarium') and
@@ -108,8 +108,10 @@ class Image:
                                    'vagrant' in _), None)
         if _name:
             if 'libvirt' in _name:
-                _type = 'libvirt'
-            return _type, outdir.joinpath(_name)
+                _provider = 'libvirt'
+            elif 'virtualbox' in _name:
+                _provider = 'virtualbox'
+            return _provider, outdir.joinpath(_name)
         else:
             raise FileNotFoundError()
 
@@ -122,5 +124,5 @@ class Image:
         return self._path
 
     @property
-    def type(self) -> str:
-        return self._type
+    def provider(self) -> str:
+        return self._provider

tools/libaqr/runner.py
@@ -66,6 +66,7 @@ class RunnerResult(BaseModel):
 class Runner(multiprocessing.Process):
 
     _boxname: str
+    _provider: str
     _test: SuiteEntry
     _run_name: Optional[str]
     _deployment: Optional[Deployment]
@@ -76,11 +77,13 @@ class Runner(multiprocessing.Process):
         self,
         deployments: Path,
         boxname: str,
+        provider: str,
         test: SuiteEntry
     ):
         super().__init__()
         print(f"create runner for {test}")
         self._boxname = boxname
+        self._provider= provider
         self._test = test
         self._deployment = None
         self._run_name = None
@@ -149,6 +152,7 @@ class Runner(multiprocessing.Process):
             self._deployment = Deployment.create(
                 name=self._run_name,
                 box=self._boxname,
+                provider=self._provider,
                 num_nodes=requirements.nodes,
                 num_disks=requirements.disks,
                 num_nics=requirements.nics,

tools/libaqr/vagrant.py
@@ -86,39 +86,43 @@ class Vagrant:
         return retcode
 
     @classmethod
-    def box_list(cls) -> List[str]:
-        """ List all known vagrant boxes, including unrelated to aquarium. """
+    def box_list(cls) -> List[Tuple[str,str]]:
+        """ List all known vagrant boxes with the provider, including unrelated to aquarium. """
 
         cmd = "vagrant box list --machine-readable"
         retcode, out, err = cls.run(None, cmd, interactive=False)
         if retcode != 0:
             raise VagrantError(msg=err, errno=retcode)
 
-        boxes = (name for name in out.splitlines()
-                 if name.count("box-name") > 0)
-        boxnames = [entry.split(",")[3] for entry in boxes]
-        return boxnames
+        boxes = [name for name in out.splitlines() if name.count("box-name") > 0]
+        providers = [provider for provider in out.splitlines() if provider.count("box-provider") > 0]
+        boxlist: List[Tuple[str,str]] = []
+        for box_entry, provider_entry in zip(boxes, providers):
+            boxname = box_entry.split(",")[3]
+            provider = provider_entry.split(",")[3]
+            boxlist.append((boxname, provider))
+        return boxlist
 
     @classmethod
-    def box_remove(cls, name: str) -> None:
+    def box_remove(cls, name: str, provider: str='libvirt') -> None:
         """ Remove an existing box """
-        if name not in cls.box_list():
+        if [name, provider] not in cls.box_list():
             return
 
-        cmd = f"vagrant box remove {name}"
+        cmd = f"vagrant box remove {name} --provider {provider}"
         retcode, _, err = cls.run(None, cmd, interactive=False)
         if retcode != 0:
             raise VagrantError(
-                msg=f"failed removing box '{name}': {err}",
+                msg=f"failed removing box '{name}' ({provider}): {err}",
                 errno=retcode
             )
 
     @classmethod
-    def box_add(cls, name: str, path: Path) -> None:
-        """ Add image from 'path' as box 'name' """
+    def box_add(cls, name: str, path: Path, provider: str='libvirt') -> None:
+        """ Add image from 'path' as box 'name', different 'provider' could have the same 'name' """
 
-        avail_boxes: List[str] = cls.box_list()
-        if name in avail_boxes:
+        avail_boxes = cls.box_list()
+        if [name, provider] in avail_boxes:
             raise BoxAlreadyExistsError()
 
         cmd = f"vagrant box add {name} {path}"

commit 53594c234815a91154390daf48a783e30f5ee196

libaqr/deployment: do not add extra disk number
After bd3d76e merged it started to add to Vagrant more disks than
required, as much as twice.

Fixes: bd3d76e

Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@suse.com>

tools/libaqr/deployment.py
@@ -269,12 +269,13 @@ Vagrant.configure("2") do |config|
                     f"""
             lv.customize ['storagectl', :id, '--name', 'SATA Controller', '--portcount', '6'] """
 
+        disk_size = '8G'
         for did in range(1, disks+1):
             if provider == "libvirt":
                 serial = "".join(random.choice("0123456789") for _ in range(8))
                 node_storage += \
                         f"""
-            lv.storage :file, size: "8G", type: "qcow2", serial: "{serial}" """
+            lv.storage :file, size: "{disk_size}", type: "qcow2", serial: "{serial}" """
             elif provider == "virtualbox":
                 node_storage += \
                         f"""
@@ -283,11 +284,6 @@ Vagrant.configure("2") do |config|
             end
             lv.customize ['storageattach', :id,  '--storagectl', 'SATA Controller', '--port', {did}, '--device', 0, '--type', 'hdd', '--medium', N{nid}DISK{did}] """
 
-            serial = "".join(random.choice("0123456789") for _ in range(8))
-            node_storage += \
-                f"""
-            lv.storage :file, size: "8G", type: "qcow2", serial: "{serial}"
-                """
 
         is_primary_str = "true" if nid == 1 else "false"
         node_str: str = \