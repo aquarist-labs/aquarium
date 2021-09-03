20e05851f04af209811f02705c741c8c7b8a9854
Tatjana Dehler <tdehler@suse.com>
2021-09-03 12:56:28 +0200

    glass: migrate sidenav from material to bootstrap
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- src/glass/src/app/core/navigation-bar/navigation-bar.component.ts
+++ src/glass/src/app/core/navigation-bar/navigation-bar.component.ts
@@ -1,3 +1,17 @@
+/*
+ * Project Aquarium's frontend (glass)
+ * Copyright (C) 2021 SUSE, LLC.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
 import { Component, OnInit } from '@angular/core';
 import { marker as TEXT } from '@biesbjerg/ngx-translate-extract-marker';
 
@@ -13,20 +27,21 @@ export class NavigationBarComponent implements OnInit {
   navItems: NavItem[] = [
     {
       name: TEXT('Dashboard'),
-      icon: 'mdi:apps',
+      icon: 'mdi mdi-apps',
       route: '/dashboard'
     },
     {
       name: TEXT('Users'),
-      icon: 'mdi:account-multiple',
+      icon: 'mdi mdi-account-multiple',
       route: '/dashboard/users'
     },
     {
       name: TEXT('Hosts'),
-      icon: 'mdi:server-network',
+      icon: 'mdi mdi-server-network',
       route: '/dashboard/hosts'
     }
   ];
+
   /* Subitem example. Can be removed as soon as we
      have an actual sub-item ;)
      {
