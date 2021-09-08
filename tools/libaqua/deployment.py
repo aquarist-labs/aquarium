9cd2b80943524cc0c9e77f008e2dc130673fc330
Kyr Shatskyy <kyrylo.shatskyy@suse.com>
2021-09-08 15:36:57 +0200

    aqua: move vagrant file generation to vagrant module
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@suse.com>

--- tools/libaqua/deployment.py
+++ tools/libaqua/deployment.py
@@ -17,7 +17,6 @@ from pathlib import Path
 import logging
 from typing import Dict, List, Optional, Tuple
 from datetime import datetime as dt
-import random
 import shutil
 import sys
 import json
@@ -111,7 +110,8 @@ class Deployment:
             else:
                 raise AqrError(f'Unknown deployment model {model_name}')
         else:
-            logger.warning(f'Cannot determine model')
+            deployment = meta.get('name') or 'unknown deployment'
+            logger.warning(f'Cannot determine model for "{deployment}"')
             return DeploymentModel.parse_obj(meta)
 
 
@@ -162,7 +162,7 @@ class Deployment:
 
         path = cls._create(name=name, deployments_path=deployments_path)
 
-        vagrantfile_text = _gen_vagrantfile(
+        vagrantfile_text = vagrant.gen_vagrantfile(
             box, provider, mount_path,
             num_nodes, num_disks, num_nics
         )
@@ -252,115 +252,6 @@ class Deployment:
                        f'{self.__class__.__name__} class')
         return []
 
-
-def _gen_vagrantfile(
-    boxname: str,
-    provider: str,
-    rootdir: Optional[Path],
-    nodes: int,
-    disks: int,
-    nics: int
-) -> str:
-
-    logger.debug(
-        f"generate template: nodes={nodes}, disks={disks}, nics={nics}"
-    )
-
-    without_shared_folder_str: str = "true" if not rootdir else "false"
-    rootdir = rootdir if rootdir else Path(".")
-
-    """ template for node storage, libvirt and virtualbox use different format """
-    create_node_storage: str = ""
-    if provider == "virtualbox":
-        create_node_storage = \
-                f"""
-vb_config = `VBoxManage list systemproperties`.split(/\\n/).grep(/Default machine folder/).first
-diskpath = vb_config.split(':', 2)[1].strip() """
-        for nid in range(1, nodes+1):
-            for did in range(1, disks+1):
-                create_node_storage += \
-                        f"""
-N{nid}DISK{did} = File.join(diskpath, '{boxname}-node{nid}', 'node{nid}-disk{did}.vdi') """
-
-
-    template: str = \
-        f"""
-# -*- mode: ruby -*-
-# vim: set ft=ruby
-{create_node_storage}
-
-Vagrant.configure("2") do |config|
-    config.vm.box = "{boxname}"
-    config.vm.synced_folder ".", "/vagrant", disabled: true
-
-    config.vm.provider :virtualbox do |_, override|
-        override.vm.synced_folder "{rootdir}", "/srv/aquarium", type: "virtualbox", disabled: {without_shared_folder_str}
-    end
-    config.vm.provider :libvirt do |_, override|
-        override.vm.synced_folder "{rootdir}", "/srv/aquarium", type: "nfs", nfs_udp: false, disabled: {without_shared_folder_str}
-    end
-
-    config.vm.guest = "suse"
-
-        """
-
-    host_port = 1337
-    for nid in range(1, nodes+1):
-
-        node_networks: str = ""
-        for _ in range(0, nics-1):
-            node_networks += \
-                """
-        node.vm.network :private_network, :type => "dhcp"
-                """
-
-        node_storage: str = ""
-        if provider == "virtualbox":
-            node_storage += \
-                    f"""
-            lv.customize ['storagectl', :id, '--name', 'SATA Controller', '--portcount', '6'] """
-
-        disk_size = '8G'
-        for did in range(1, disks+1):
-            if provider == "libvirt":
-                serial = "".join(random.choice("0123456789") for _ in range(8))
-                node_storage += \
-                        f"""
-            lv.storage :file, size: "{disk_size}", type: "qcow2", serial: "{serial}" """
-            elif provider == "virtualbox":
-                node_storage += \
-                        f"""
-            unless File.exist?(N{nid}DISK{did})
-                lv.customize ['createhd', '--filename', N{nid}DISK{did}, '--format', 'VDI', '--size', '20000' ]
-            end
-            lv.customize ['storageattach', :id,  '--storagectl', 'SATA Controller', '--port', {did}, '--device', 0, '--type', 'hdd', '--medium', N{nid}DISK{did}] """
-
-
-        is_primary_str = "true" if nid == 1 else "false"
-        node_str: str = \
-            f"""
-    config.vm.define :"node{nid}", primary: {is_primary_str} do |node|
-        node.vm.hostname = "node{nid}"
-        node.vm.network "forwarded_port", guest: 1337, host: {host_port}, host_ip: "*"
-        {node_networks}
-
-        node.vm.provider "libvirt" do |lv|
-            lv.memory = 4096
-            lv.cpus = 1
-            {node_storage}
-        end
-    end
-            """
-
-        template += node_str
-        host_port += 1
-
-    template += \
-        """
-end
-        """
-
-    return template
 
 
 def get_deployments(path: Path) -> Dict[str, Deployment]:
