953dd3d918f998a7f248f53dd759bea5763c2964
Tatjana Dehler <tdehler@suse.com>
2021-09-09 10:55:58 +0200

    glass: remove host widget (for now)
    Signed-off-by: Tatjana Dehler <tdehler@suse.com>

--- src/glass/src/app/core/dashboard/dashboard.module.ts
+++ src/glass/src/app/core/dashboard/dashboard.module.ts
@@ -7,7 +7,6 @@ import { TranslateModule } from '@ngx-translate/core';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
 import { HealthDashboardWidgetComponent } from '~/app/core/dashboard/widgets/health-dashboard-widget/health-dashboard-widget.component';
-import { HostsDashboardWidgetComponent } from '~/app/core/dashboard/widgets/hosts-dashboard-widget/hosts-dashboard-widget.component';
 import { PerformanceDashboardWidgetComponent } from '~/app/core/dashboard/widgets/performance-dashboard-widget/performance-dashboard-widget.component';
 import { SysInfoDashboardWidgetComponent } from '~/app/core/dashboard/widgets/sys-info-dashboard-widget/sys-info-dashboard-widget.component';
 import { VolumesDashboardWidgetComponent } from '~/app/core/dashboard/widgets/volumes-dashboard-widget/volumes-dashboard-widget.component';
@@ -19,14 +18,12 @@ import { SharedModule } from '~/app/shared/shared.module';
     VolumesDashboardWidgetComponent,
     HealthDashboardWidgetComponent,
     SysInfoDashboardWidgetComponent,
-    HostsDashboardWidgetComponent,
     PerformanceDashboardWidgetComponent
   ],
   exports: [
     VolumesDashboardWidgetComponent,
     HealthDashboardWidgetComponent,
     SysInfoDashboardWidgetComponent,
-    HostsDashboardWidgetComponent,
     PerformanceDashboardWidgetComponent
   ],
   imports: [
