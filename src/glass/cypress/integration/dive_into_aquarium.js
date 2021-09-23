382a604745dc306fe488eef795e120ffd8e73496
Tim Serong <tserong@suse.com>
2021-09-23 14:26:15 +1000

    glass: make cypress wait for elements to be visible
    This adds a bunch of ".should('be.visible')" calls to slow cypress
    down a bit, and wait for each page of the setup wizard to render
    completely before trying to hit the Next/Install buttons.
    
    Signed-off-by: Tim Serong <tserong@suse.com>

--- src/glass/cypress/integration/dive_into_aquarium.js
+++ src/glass/cypress/integration/dive_into_aquarium.js
@@ -1,8 +1,9 @@
 describe('Dive into Aquarium', () => {
   it('Visit the first tank', () => {
     cy.visit('/');
-    cy.contains('Create new cluster');
+    cy.contains('Create new cluster').should('be.visible');
     cy.clickButton('Create');
+    cy.contains('This wizard will guide you through the installation process').should('be.visible');
     cy.contains('Start');
     cy.contains('Networking');
     cy.contains('Time');
@@ -11,12 +12,14 @@ describe('Dive into Aquarium', () => {
     cy.contains('Finish');
     cy.contains('Next');
     cy.clickButton('Next');
-    cy.contains('Hostname');
+    cy.contains('Hostname').should('be.visible');
     cy.contains('div', 'Hostname').find('input').type('foohost{enter}');
     cy.clickButton('Next');
-    cy.contains('Use an NTP host on the Internet').click();
+    cy.contains('Use an NTP host on the Internet').should('be.visible').click();
     cy.clickButton('Next');
+    cy.contains('The following devices have been identified in the system').should('be.visible');
     cy.clickButton('Next');
+    cy.contains('Aquarium will now install and setup the core system.').should('be.visible');
     cy.clickButton('Install');
   });
 });
